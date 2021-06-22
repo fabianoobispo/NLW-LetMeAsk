23ed0f5e4b1a89e12f5d13e1e2aae74f994e445d
fabianoobispo <fbc623@gmail.com>
2021-06-21 21:01:05 -0300

    primeiro commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import IlustrationImg from '../assets/images/illustration.svg'
+
+
+export function Home(){
+    return(
+        <div>
+            <aside>
+                <img src={IlustrationImg}  alt='logo'/>
+            </aside>
+        </div>
+    )
+}
